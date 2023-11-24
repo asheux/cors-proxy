66b4e934daf10e2c08f023be4afd5445b927a1a0
asheux <brian.mboya@protonmail.com>
2023-11-25 00:04:02 +0300

    host proxy server

--- proxy.py
+++ proxy.py
@@ -3,16 +3,23 @@ import requests
 from flask import Flask, request
 from flask_cors import CORS
 
-from settings import *
-
-
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/crawl', methods=['GET'])
 def crawl():
-    url = request.args.get("url") # get url to crawl
-    res = requests.get(url)
+    message = ""
+    try:
+        url = request.args.get("url") # get url to crawl
+        res = requests.get(url)
+
+    except Exception as error:
+        res = None
+        message = f"Error making request: {error}"
+
+    if res is None:
+        return message
+
     if res.status_code == 200:
         body = res.text
     else:
@@ -20,4 +27,4 @@ def crawl():
     return body
 
 if __name__ == "__main__":
-    app.run(debug=APP_DEBUG)
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- settings.py
+++
@@ -1,8 +0,0 @@
-"""
-imports
-"""
-import os
-
-HEADERS = {'content-type': 'application/json'}
-DEVELOPMENT = True
-APP_DEBUG = True
